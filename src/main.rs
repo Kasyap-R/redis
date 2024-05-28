35bbe07431a913bbf69c36d9b177520443ae6063
Kasyap-R <kasyap.rachuri2@gmail.com>
2024-05-27 22:36:39 -0700

    Added multithreading to deal with concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,23 +10,24 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buf = [0; 512];
-                loop {
-                    // read_count is the num of bytes sent
-                    let num_bytes = stream.read(&mut buf).unwrap();
-                    if num_bytes == 0 {
-                        break;
+        let _handle = thread::spawn(move || {
+            match stream {
+                Ok(mut stream) => {
+                    println!("accepted new connection");
+                    let mut buf = [0; 512];
+                    loop {
+                        // num_bytes is the num of bytes sent
+                        let num_bytes = stream.read(&mut buf).unwrap();
+                        if num_bytes == 0 {
+                            break;
+                        }
+                        stream.write(b"+PONG\r\n").unwrap();
                     }
-                    stream.write(b"+PONG\r\n").unwrap();
+                }
+                Err(e) => {
+                    println!("error: {}", e);
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
-                break;
-            }
-        }
+        });
     }
 }
